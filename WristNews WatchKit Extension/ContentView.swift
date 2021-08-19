4c116f733b6adef6306b0d6f3b04ccfd93cb1797
zfachrian <zuhadachmadfachrian@gmail.com>
2021-08-19 09:04:19 +0700

    Initial Commit

---
+++ WristNews WatchKit Extension/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WristNews WatchKit Extension
+//
+//  Created by Zuhad Fachrian on 19/08/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WristNews WatchKit Extension/WristNewsApp.swift
@@ -0,0 +1,19 @@
+//
+//  WristNewsApp.swift
+//  WristNews WatchKit Extension
+//
+//  Created by Zuhad Fachrian on 19/08/21.
+//
+
+import SwiftUI
+
+@main
+struct WristNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}
